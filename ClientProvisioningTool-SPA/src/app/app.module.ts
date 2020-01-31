c69b7dc766f9e0be9c7c7e9f3d80086e925f8f95
Jonathan Eren <Jonathan.Eren@pinkerton.com>
2020-01-31 14:10:35 -0800

    Add Bootstrap Formatting Libs

--- ClientProvisioningTool-SPA/src/app/app.module.ts
+++ ClientProvisioningTool-SPA/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { BsDropdownModule } from 'ngx-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
@@ -18,7 +20,9 @@ import { ErrorInteceptorProvider } from './_services/error.interceptor';
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      BrowserAnimationsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       ErrorInteceptorProvider
